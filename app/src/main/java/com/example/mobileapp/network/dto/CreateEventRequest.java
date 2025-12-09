b5c094a4dbbc0fa651372383678a6cb4e9b17055
523h0020-cyber <523h0020@student.tdtu.edu.vn>
2025-12-09 19:11:19 +0700

    Add address, end_time, video_url fields to CreateEventRequest DTO

--- app/src/main/java/com/example/mobileapp/network/dto/CreateEventRequest.java
+++ app/src/main/java/com/example/mobileapp/network/dto/CreateEventRequest.java
@@ -6,8 +6,11 @@ public class CreateEventRequest {
     public String title;
     public String description;
     public String start_time;
+    public String end_time;
     public String location_name;
+    public String address;
     public String thumbnail_url;
+    public String video_url;
     public int category_id;
     public List<TicketTypeDto> ticket_types;
 }
